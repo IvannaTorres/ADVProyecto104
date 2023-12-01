f7cfa82f2a5eee66a068c3933a673ffb356d1655
IvannaTorres <119988894+IvannaTorres@users.noreply.github.com>
2023-11-30 19:49:43 -0500

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -1,20 +1,22 @@
+//Obtener nombre del jugador
+player_name = localStorage.getItem("player_name");
 // valor aleatorio generado
 var y = Math.floor(Math.random() * 10 + 1);
 // contar el número de intentos
-var guess = 0;
-// crear para el intento correcto
-
+var guess = 1;
 // función para el número adivinado por el usuario     
 function submit(){
+    //numero adivinado por el usuario
     var x = document.getElementById("guessField").value;
+    // condicional para el intento correcto
     if(x == y){
-        alert("Felicitaciones!!" + playername + "lo adivinaste bien" + guess + "intento");
+        alert("Felicitaciones " +player_name+ " lo adivinaste bien!! " +guess+ " intento!!");
         guess = 1;
-    }
+    } //si el número adivinado es mayor que el número actual
     else if(x > y){
         guess++;
         alert("Ups lo siento intenta con un número mas pequeño!!");
-    }
+    } //si el número adivinado es menor que el número actual
     else{
         guess++;
         alert("Ups lo siento intenta con un número mas grande!!");
